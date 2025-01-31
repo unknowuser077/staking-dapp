4c7bdc182271fbf512f431db4e21cb9de55449d8
mr.toxic <unknow.7075@gmail.com>
2025-01-31 23:31:46 +0330

    use Reacr Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { Landing } from './pages/landing'
-
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './index.css';
+import { Landing } from './pages/landing';
+import { Dashboard } from './pages/dashboard/Dashboard';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Landing />
-    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
